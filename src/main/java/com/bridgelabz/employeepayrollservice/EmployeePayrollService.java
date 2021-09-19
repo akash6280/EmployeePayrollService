2f90c730bbab1777baec6a1cfddadbb1e00de8be
Akash Singh <akashsingh6280@gmail.com>
2021-09-20 03:32:59 +0530

    ADDED: method to read employee payroll data from console

--- src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollService.java
@@ -15,12 +15,23 @@ public class EmployeePayrollService {
     public EmployeePayrollService(ArrayList<EmployeePayrollData> employeePayrollList) {
         this.employeePayrollList = employeePayrollList;
     }
+    
+    public void readEmployeePayrollData(Scanner consoleInputReader) {
+        System.out.println("Enter Employee ID: ");
+        int id = consoleInputReader.nextInt();
+        System.out.println("Enter Employee Name: ");
+        String name = consoleInputReader.next();
+        System.out.println("Enter Employee Salary: ");
+        double salary = consoleInputReader.nextDouble();
+        employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+    }
 
   
     public static void main(String[] args) {
         ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
         EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
-      
+        Scanner consoleInputReader = new Scanner(System.in);
+        employeePayrollService.readEmployeePayrollData(consoleInputReader);
     }
 } 
 

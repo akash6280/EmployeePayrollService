382e51d6dc1bd1e6a3eba3a08a8d4682e02e5ed3
Akash Singh <akashsingh6280@gmail.com>
2021-09-27 04:36:54 +0530

    ADDED: function to get employee payroll data

--- src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollDBService.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollDBService.java
@@ -2,6 +2,7 @@ package com.bridgelabz.employeepayrollservice;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,6 +11,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeePayrollDBService {
+	
+	private PreparedStatement employeePayrollDataStatement;
+    private static EmployeePayrollDBService employeePayrollDBService;
+
+    public static EmployeePayrollDBService getInstance() {
+        if (employeePayrollDBService == null)
+            employeePayrollDBService = new EmployeePayrollDBService();
+        return employeePayrollDBService;
+    }
+
+    public EmployeePayrollDBService() {
+
+    }
+
+	
 
 	private Connection getConnection() throws SQLException {
 		String jdbcURL="jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
@@ -56,6 +72,45 @@ public class EmployeePayrollDBService {
 		}
 		return 0;
 	}
+	
+	public List<EmployeePayrollData> getEmployeePayrollDataFromDB(String name) {
+        List<EmployeePayrollData> employeePayrollList = null;
+        if (this.employeePayrollDataStatement == null)
+            this.prepareStatementForEmployeeData();
+        try {
+            employeePayrollDataStatement.setString(1, name);
+            ResultSet resultSet = employeePayrollDataStatement.executeQuery();
+            employeePayrollList = this.getEmployeePayrollData(resultSet);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return employeePayrollList;
+    }
+
+    private List<EmployeePayrollData> getEmployeePayrollData(ResultSet resultSet) {
+        List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+        try {
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                double salary = resultSet.getDouble("basic_pay");
+                employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return employeePayrollList;
+    }
+
+    private void prepareStatementForEmployeeData() {
+        try {
+            Connection connection = this.getConnection();
+            String sql = "select id, name, basic_pay from employee_payroll where name=?";
+            employeePayrollDataStatement = connection.prepareStatement(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }

86cd18413c5d603266ce35b57df5e7bdee359880
Akash Singh <akashsingh6280@gmail.com>
2021-09-27 02:39:57 +0530

    ADDED: construtor to initialise new parameter date

--- src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollData.java
@@ -3,16 +3,26 @@
  */
 package com.bridgelabz.employeepayrollservice;
 
+import java.time.LocalDate;
+
 public class EmployeePayrollData {
 
 		public int id;
 		public String name;
 		public double salary;
+		public LocalDate startDate;
 
 		public EmployeePayrollData(int id, String name, double salary) {
 			this.id = id;
 			this.name = name;
 			this.salary = salary;
+		}
+
+		public EmployeePayrollData(int id, String name, Double salary, LocalDate startDate) {
+			this.id = id;
+	        this.name = name;
+	        this.salary = salary;
+	        this.startDate = startDate;
 		}
 
 		@Override

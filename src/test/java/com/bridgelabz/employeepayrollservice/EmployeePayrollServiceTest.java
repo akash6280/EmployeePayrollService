ce07ff90ac6e0423aaa728181406e832c3ae0e4b
Akash Singh <akashsingh6280@gmail.com>
2021-09-27 07:38:19 +0530

    ADDED: test case to find min salary of employee of both gender and verify it

--- src/test/java/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceTest.java
@@ -87,5 +87,16 @@ public class EmployeePayrollServiceTest {
 
 	}
     
+    @Test
+    public void givenEmployeePayrollInDB_ShouldRetrieveEmployeeMinimumSalaryWithGender(){
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		Map<String, Double> expectedGenderSalaryMap = new HashMap<String, Double>();
+		expectedGenderSalaryMap.put("F", 1000000.00);
+		expectedGenderSalaryMap.put("M", 3000000.00);
+		Map<String, Double> actualgenderSalaryMap = employeePayrollService.getMinimumSalaryBasedOnGender();
+		Assert.assertEquals(expectedGenderSalaryMap, actualgenderSalaryMap);
+
+	}
+    
     
 }

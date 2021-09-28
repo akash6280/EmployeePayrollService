e52a46d9a1db3afb7e3d61a4d1f7a4086b1dcb90
Akash Singh <akashsingh6280@gmail.com>
2021-09-29 03:53:29 +0530

    ADDED: function to add employee and payroll detail to database

--- src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollDBService.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollDBService.java
@@ -63,7 +63,7 @@ public class EmployeePayrollDBService {
 		return employeePayrollList;
 	}
 	
-	public EmployeePayrollData addEmployeeToPayroll(String name, double salary, LocalDate startDate, char gender) {
+	public EmployeePayrollData addEmployeeToPayrollUC7(String name, double salary, LocalDate startDate, char gender) {
 		int employeeId = -1;
 		EmployeePayrollData employeePayrollData = null;
 		String sql = String.format("insert into employee_payroll(name,basic_pay, start,gender) values('%s','%s','%s','%c' )", name,salary, Date.valueOf(startDate),gender );
@@ -81,7 +81,50 @@ public class EmployeePayrollDBService {
 		}
 		return employeePayrollData;
 	}
+     
+	public EmployeePayrollData addEmployeeToPayroll(String name, double salary, LocalDate startDate, char gender){
+		int employeeId = -1;
+		Connection connection = null;
+		EmployeePayrollData employeePayrollData = null;
+		try{
+			connection = this.getConnection();
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
 
+		try (Statement statement = connection.createStatement()){
+
+			String sql = String.format("insert into employee_payroll (name, basic_pay, start, gender ) VALUES ('%s', '%s', '%s', '%s');", name,salary, Date.valueOf(startDate), gender );
+
+			int rowAffected = statement.executeUpdate(sql, statement.RETURN_GENERATED_KEYS);
+			if(rowAffected == 1) {
+				ResultSet resultSet = statement.getGeneratedKeys();
+				if(resultSet.next()) employeeId = resultSet.getInt(1);
+			}  
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+
+		try(Statement statement = connection.createStatement()){
+
+			double deductions = salary * 0.2;
+			double taxablePay = salary - deductions;
+			double tax = taxablePay * 0.1;
+			double netPay = salary - tax;
+			String sql = String.format("insert into payroll_details (id, basic_pay, deductions, taxable_pay, tax, net_pay)"
+					     + "values ('%s', '%s', '%s', '%s', '%s','%s')",employeeId, salary, deductions, taxablePay, tax, netPay);
+			int rowAffected = statement.executeUpdate(sql);
+			if (rowAffected == 1) {
+				employeePayrollData = new EmployeePayrollData(employeeId, name, salary, startDate);
+			}			
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return employeePayrollData;
+	}
+	
 	public int updateEmployeeData(String name, double salary) {
 		return this.updateEmployeeDataUsingPreparedStatement(name,salary);
 	}
@@ -103,6 +146,7 @@ public class EmployeePayrollDBService {
 	        }
 			return 0;
 	}
+	
 	private void prepareStatementForUpdatingEmployeeData() {
         try {
             Connection connection = this.getConnection();

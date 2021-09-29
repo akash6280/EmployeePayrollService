029756343f239a25ccaa8f8034374008e6bd954c
Akash Singh <akashsingh6280@gmail.com>
2021-09-29 08:13:50 +0530

    MODIFIED: removed test cases of file IO services

--- src/test/java/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceTest.java
@@ -14,21 +14,6 @@ import com.bridgelabz.employeepayrollservice.EmployeePayrollService.I0Service;
 public class EmployeePayrollServiceTest {
 
     @Test
-    public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
-        EmployeePayrollData[] arrayOfEmps = {
-                new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
-                new EmployeePayrollData(2, "Bill Gates", 200000.0),
-                new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0)
-        };
-        EmployeePayrollService employeePayrollService;
-        employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
-        employeePayrollService.writeEmployeePayrollData(I0Service.FILE_I0);
-		employeePayrollService.printData(I0Service.FILE_I0);
-		long entries = employeePayrollService.countEntries(I0Service.FILE_I0);
-		Assert.assertEquals(3, entries);
-    }
-    
-    @Test
 	public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount(){
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
 		List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(I0Service.DB_I0);
